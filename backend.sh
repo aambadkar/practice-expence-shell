4ec450de5afc02e1d9cb96c471c383b9bf9c6801
aambadkar <khodaskar.ankita@gmail.com>
2023-11-21 09:42:27 +0200

    shell practice

--- backend.sh
+++ backend.sh
@@ -6,7 +6,7 @@ cp backend.service /etc/systemd/system/backend.service
 dnf install nodejs -y
 
 useradd expense
-
+rm -rf /app
 mkdir /app
 
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
@@ -26,4 +26,4 @@ systemctl start backend
 
 dnf install mysql -y
 
-mysql -h 172.31.42.80 -uroot -pExpenseApp@1 < /app/schema/backend.sql
+mysql -h mysql.devopsa17.online -uroot -pExpenseApp@1 < /app/schema/backend.sql
