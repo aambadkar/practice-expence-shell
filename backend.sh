6f25998a0d1b8f1bdc42b06a17220c7130dbaf58
aambadkar <khodaskar.ankita@gmail.com>
2023-11-22 10:08:49 +0200

    shell practice

--- backend.sh
+++ backend.sh
@@ -1,36 +1,47 @@
 source common.sh
 component=backend
+
 echo disable node js 10 version
 dnf module disable nodejs -y >>$log_file
 dnf module enable nodejs:18 -y >>$log_file
+echo $?
 
 echo copy backend service file
 cp backend.service /etc/systemd/system/backend.service >>$log_file
+echo $?
 
 echo install nodeJS
 dnf install nodejs -y >>$log_file
+echo $?
 
 echo add application user
 useradd expense >>$log_file
+echo $?
 
 echo clean app contents
 rm -rf /app >>$log_file
 mkdir /app
+echo $?
 
 cd /app
 
 download_and_extract
+echo $?
 
 echo download dependencies
 npm install >>$log_file
+echo $?
 
 echo start backend service
 systemctl daemon-reload >>$log_file
 systemctl enable backend >>$log_file
 systemctl start backend >>$log_file
+echo $?
 
 echo install mysql client
 dnf install mysql -y >>$log_file
+echo $?
 
 echo load the schema
 mysql -h mysql.devopsa17.online -uroot -pExpenseApp@1 < /app/schema/backend.sql >>$log_file
+echo $?
