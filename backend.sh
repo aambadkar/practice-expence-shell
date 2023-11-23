991cd7ac864d81f7a590d4298e4a48538703550f
aambadkar <khodaskar.ankita@gmail.com>
2023-11-23 10:00:17 +0200

    shell practice

--- backend.sh
+++ backend.sh
@@ -1,47 +1,54 @@
 source common.sh
 component=backend
 
-echo disable node js 10 version
-dnf module disable nodejs -y >>$log_file
-dnf module enable nodejs:18 -y >>$log_file
+echo install nodejs repo
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash >>$log_file
 if [ $? -eq 0 ]; then
-   echo SUCCESS
+   echo -e "\e[32mSUCCESS\e[0m"
 else
-   echo FAILED
+   echo -e "\e[31mSUCCESS\e[0m"
 fi
+#echo disable node js 10 version
+#dnf module disable nodejs -y >>$log_file
+#dnf module enable nodejs:18 -y >>$log_file
+#if [ $? -eq 0 ]; then
+   #echo SUCCESS
+#else
+   #echo FAILED
+#fi
 
 
 echo install nodeJS
 dnf install nodejs -y >>$log_file
 if [ $? -eq 0 ]; then
-   echo SUCCESS
+   echo -e "\e[32mSUCCESS\e[0m"
 else
-   echo FAILED
+   echo -e "\e[31mSUCCESS\e[0m"
 fi
 
 echo copy backend service file
 cp backend.service /etc/systemd/system/backend.service >>$log_file
 if [ $? -eq 0 ]; then
-   echo SUCCESS
+   echo -e "\e[32mSUCCESS\e[0m"
 else
-   echo FAILED
+   echo -e "\e[31mSUCCESS\e[0m"
 fi
 
 
 echo add application user
 useradd expense >>$log_file
 if [ $? -eq 0 ]; then
-   echo SUCCESS
+   echo -e "\e[32mSUCCESS\e[0m"
 else
-   echo FAILED
+   echo -e "\e[31mSUCCESS\e[0m"
 fi
 
 echo clean app contents
 rm -rf /app >>$log_file
 if [ $? -eq 0 ]; then
-   echo SUCCESS
+   echo -e "\e[32mSUCCESS\e[0m"
 else
-   echo FAILED
+   echo -e "\e[31mSUCCESS\e[0m"
 fi
 
 mkdir /app
@@ -55,7 +62,7 @@ npm install &>>$log_file
 if [ $? -eq 0 ]; then
    echo -e "\e[32mSUCCESS\e[0m"
 else
-   echo "\e[31mSUCCESS\e[0m"
+   echo -e "\e[31mSUCCESS\e[0m"
 fi
 
 echo start backend service
@@ -63,23 +70,23 @@ systemctl daemon-reload >>$log_file
 systemctl enable backend >>$log_file
 systemctl start backend >>$log_file
 if [ $? -eq 0 ]; then
-   echo SUCCESS
+   echo -e "\e[32mSUCCESS\e[0m"
 else
-   echo FAILED
+   echo -e "\e[31mSUCCESS\e[0m"
 fi
 
 echo install mysql client
 dnf install mysql -y >>$log_file
 if [ $? -eq 0 ]; then
-   echo SUCCESS
+   echo -e "\e[32mSUCCESS\e[0m"
 else
-   echo FAILED
+   echo -e "\e[31mSUCCESS\e[0m"
 fi
 
 echo load the schema
 mysql -h mysql.devopsa17.online -uroot -pExpenseApp@1 < /app/schema/backend.sql >>$log_file
-if [ $? -eq 0 ]; then
-   echo SUCCESS
-else
-   echo FAILED
-fi
+iif [ $? -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+ else
+    echo -e "\e[31mSUCCESS\e[0m"
+ fi
