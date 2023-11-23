ff2f41f3be4094b356e4101cb117b22900b61438
aambadkar <khodaskar.ankita@gmail.com>
2023-11-23 10:39:49 +0200

    shell practice

--- backend.sh
+++ backend.sh
@@ -1,21 +1,19 @@
-source common.sh
+source  common.sh
 component=backend
 
-echo install nodejs repo
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash >>$log_file
-stat_check
-echo disable node js 10 version
-dnf module disable nodejs -y >>$log_file
-dnf module enable nodejs:18 -y >>$log_file
-stat_check
+type npm &>>log_file
+  if [ $? -ne 0 ]; then
+  echo install nodJS Repos
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash >>$log_file
+  stat_check
 
-
-echo install nodeJS
-dnf install nodejs -y >>$log_file
-stat_check
+  echo install nodeJS
+  dnf install nodejs -y &>>$log_file
+  stat_check
+fi
 
 echo copy backend service file
-cp backend.service /etc/systemd/system/backend.service >>$log_file
+cp backend.service /etc/systemd/system/backend.service &>>$log_file
 stat_check
 
 echo add application user
@@ -25,8 +23,8 @@ if [ $? -ne 0 ]; then
 fi
 stat_check
 
-echo clean app contents
-rm -rf /app >>$log_file
+echo clean app contend
+rm -rf /app &>>$log_file
 stat_check
 
 mkdir /app
@@ -34,21 +32,80 @@ cd /app
 
 download_and_extract
 
-
 echo download dependencies
 npm install &>>$log_file
 stat_check
 
 echo start backend service
-systemctl daemon-reload >>$log_file
-systemctl enable backend >>$log_file
-systemctl start backend >>$log_file
+systemctl daemon-reload &>>$log_file
+systemctl enable backend &>>$log_file
+systemctl restart backend &>>$log_file
 stat_check
 
 echo install mysql client
-dnf install mysql -y >>$log_file
+dnf install mysql -y &>>$log_file
 stat_check
 
 echo load the schema
-mysql -h mysql.devopsa17.online -uroot -pExpenseApp@1 < /app/schema/backend.sql >>$log_file
+mysql_root_password=$1
+mysql -h mysql.devopsa17.online -uroot -p$mysql_root_password < /app/schema/backend.sql &>>$log_file
 stat_check
+
+
+
+
+#source common.sh
+#component=backend
+#
+#echo install nodejs repo
+#curl -sL https://rpm.nodesource.com/setup_lts.x | bash >>$log_file
+#stat_check
+#echo disable node js 10 version
+#dnf module disable nodejs -y >>$log_file
+#dnf module enable nodejs:18 -y >>$log_file
+#stat_check
+#
+#
+#echo install nodeJS
+#dnf install nodejs -y >>$log_file
+#stat_check
+#
+#echo copy backend service file
+#cp backend.service /etc/systemd/system/backend.service >>$log_file
+#stat_check
+#
+#echo add application user
+#id expense &>>log_file
+#if [ $? -ne 0 ]; then
+#  useradd expense &>>log_file
+#fi
+#stat_check
+#
+#echo clean app contents
+#rm -rf /app >>$log_file
+#stat_check
+#
+#mkdir /app
+#cd /app
+#
+#download_and_extract
+#
+#
+#echo download dependencies
+#npm install &>>$log_file
+#stat_check
+#
+#echo start backend service
+#systemctl daemon-reload >>$log_file
+#systemctl enable backend >>$log_file
+#systemctl start backend >>$log_file
+#stat_check
+#
+#echo install mysql client
+#dnf install mysql -y >>$log_file
+#stat_check
+#
+#echo load the schema
+#mysql_root_password=$1
+#mysql -h mysql.devopsa17.online -uroot -p$mysql_root_password < /app/schema/backend.sql >>$log_file
+#stat_check
