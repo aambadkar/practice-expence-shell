93fa60acc195978a8434579b14f8c7668bea13c8
aambadkar <khodaskar.ankita@gmail.com>
2023-11-23 10:41:17 +0200

    shell practice

--- common.sh
+++ common.sh
@@ -1,19 +1,19 @@
 log_file=/tmp/expense.log
 
 download_and_extract() {
-  echo download $component code
-  curl -s -o /tmp/$component.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip >>$log_file
+ echo download $component code
+ curl -o /tmp/$component.zip https://expense-artifacts.s3.amazonaws.com/$component.zip >>log_file
   stat_check
-
-  echo extracting $component code
-  unzip /tmp/$component.zip >>$log_file
-   stat_check
+ echo extracting $component code
+ unzip /tmp/$component.zip >>$log_file
+ stat_check
 }
-stat_check() {
+
+stat_check(){
   if [ $? -eq 0 ]; then
-     echo -e "\e[32mSUCCESS\e[0m"
+    echo -e "\e[32mSUCCESS\e[0m"
   else
-     echo -e "\e[31mFAILED\e[0m"
-     exit 1
+    echo -e "\e[31mFAILED\e[0m"
+    exit 1
   fi
 }
