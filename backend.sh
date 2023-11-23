50e856de26a1a9c2f252a2dd5acbc90b032c3e41
aambadkar <khodaskar.ankita@gmail.com>
2023-11-23 09:38:29 +0200

    shell practice

--- backend.sh
+++ backend.sh
@@ -4,7 +4,11 @@ component=backend
 echo disable node js 10 version
 dnf module disable nodejs -y >>$log_file
 dnf module enable nodejs:18 -y >>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+   echo SUCCESS
+else
+   echo FAILED
+fi
 
 
 echo install nodeJS
@@ -17,16 +21,28 @@ fi
 
 echo copy backend service file
 cp backend.service /etc/systemd/system/backend.service >>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+   echo SUCCESS
+else
+   echo FAILED
+fi
 
 
 echo add application user
 useradd expense >>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+   echo SUCCESS
+else
+   echo FAILED
+fi
 
 echo clean app contents
 rm -rf /app >>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+   echo SUCCESS
+else
+   echo FAILED
+fi
 
 mkdir /app
 cd /app
@@ -36,18 +52,34 @@ download_and_extract
 
 echo download dependencies
 npm install &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+   echo SUCCESS
+else
+   echo FAILED
+fi
 
 echo start backend service
 systemctl daemon-reload >>$log_file
 systemctl enable backend >>$log_file
 systemctl start backend >>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+   echo SUCCESS
+else
+   echo FAILED
+fi
 
 echo install mysql client
 dnf install mysql -y >>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+   echo SUCCESS
+else
+   echo FAILED
+fi
 
 echo load the schema
 mysql -h mysql.devopsa17.online -uroot -pExpenseApp@1 < /app/schema/backend.sql >>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+   echo SUCCESS
+else
+   echo FAILED
+fi
--- common.sh
+++ common.sh
@@ -3,9 +3,17 @@ log_file=/tmp/expense.log
 download_and_extract() {
   echo download $component code
   curl -s -o /tmp/$component.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip >>$log_file
-  echo $?
+if [ $? -eq 0 ]; then
+   echo SUCCESS
+else
+   echo FAILED
+fi
 
   echo extracting $component code
   unzip /tmp/$component.zip >>$log_file
-  echo $?
+ if [ $? -eq 0 ]; then
+    echo SUCCESS
+ else
+    echo FAILED
+ fi
 }
